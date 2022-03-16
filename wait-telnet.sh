59069269950b5577044e2eff7029b2b19a03b710
Menci <huanghaorui301@gmail.com>
2022-03-16 20:00:11 +0800

    Make `grep` line-buffered in wait-telnet.sh

--- wait-telnet.sh
+++ wait-telnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Waiting for QEMU serial console...
-while true; do telnet 127.0.0.1 1235 2>/dev/null | grep -v '^Trying 127.0.0.1'; sleep 0.5; done
+while true; do telnet 127.0.0.1 1235 2>/dev/null | grep --line-buffered -v '^Trying 127.0.0.1'; sleep 0.5; done
